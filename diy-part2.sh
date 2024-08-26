cfd646b1cb55740a4b6f5c1107f619758bef941e
dexxx-x <atmosfear85@gmail.com>
2024-08-27 02:31:33 +0500

    Add files via upload

---
+++ diy-part2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/OpenWrt-xor/g' package/base-files/files/bin/config_generate
+
+#sed -i 's/disabled/okay/g' /workdir/openwrt/target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4c.dts
